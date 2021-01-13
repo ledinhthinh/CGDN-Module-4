fe2b0a132a9bf5cd389839faca7a5fef2facf13c
ledinhthinh <ldt051202@gmail.com>
2021-01-13 10:36:22 +0700

    up

---
+++ 8.Validation/thuc_hanh/custom-phone-validation/src/main/java/vn/codegym/ValidApplication.java
@@ -0,0 +1,22 @@
+package vn.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@SpringBootApplication
+public class ValidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidApplication.class, args);
+    }
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("ValidationMessages");
+        return messageSource;
+    }
+
+}
